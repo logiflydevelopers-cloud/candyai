e6de817803ada705231b008554da264e1918208d
sellchatai <sellchatsai@gmail.com>
2026-02-20 16:31:56 +0530

    add nodemailer dependency

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,9 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import "./styles/theme.css";
 import "./styles/layout.css";
 import "./index.css";
@@ -8,11 +12,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
 
 reportWebVitals();
